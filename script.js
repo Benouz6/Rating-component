96644c4f0a6c2e6d3d8e15651f7fb5f362e7208c
Benoit Leboucher <ben.lebou@gmail.com>
2023-02-05 01:11:23 -0500

    Work on the submit

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const btns = document.getElementsByClassName("btn");
 const rate = document.getElementById('rate')
 
-for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function () {
-    const current = document.getElementsByClassName('active')
-    if (current.length > [0]) {
-      current[0].className = current[0].className.replace(' active', '')
-    }
-    this.className += ' active';
-    rate.textContent = this.textContent;
-  })
-}
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener('click', function () {
+//     const current = document.getElementsByClassName('active')
+//     if (current.length > [0]) {
+//       current[0].className = current[0].className.replace(' active', '')
+//     }
+//     this.className += ' active';
+//     rate.textContent = this.textContent;
+//   })
+// }
+
+
 
 function submit() {
+  const rating = document.getElementsByName
   const modal = document.getElementById('modal');
   const rates = document.getElementById('rates');
   modal.classList.toggle('modal');
